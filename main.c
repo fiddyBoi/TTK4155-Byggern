76177da03259b1821d38730ae519829d33e3e148
Fredrik Bratberg <fredrik.b.bratberg@gmail.com>
2024-09-05 13:52:52 +0200

    IO toggle and copied uart example

--- main.c
+++ main.c
@@ -5,16 +5,35 @@
  * Author : fredrbb
  */ 
 
-#include <avr/io.h>
-#include "uart.h"
+#define F_CPU 4915200
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include "uart.h"
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+//     /* Replace with your application code */
+// 	UART_Init();
+//     while (1) 
+//     {
+// 		unsigned char echo = UART_Receive();
+// 		UART_Transmit(echo);
+//     }
+
+int i = 0; 
+DDRA = 0x1; // set PA0 to output
+while(1){
+	if(i == 0){
+		PORTA = 0x0; // set PA0 low
+		i = 1; 
+	}else{
+		PORTA = 0x1; // set PA0 high
+		i = 0; 
+	}
+	_delay_ms(1000);
+}
+
 }
 
  
--- uart.c
+++ uart.c
@@ -1,13 +1,54 @@
 #include "uart.h"
 
 // Includes
+// ****************************************************
+// ****************************************************
 
 // Private macros and constants 
+// ****************************************************
+// ****************************************************
 
 // Private types 
+// ****************************************************
+// ****************************************************
 
 // Private global variables 
+// ****************************************************
+// ****************************************************
 
 // Private functions 
+// ****************************************************
+// ****************************************************
 
 // Implementation of the public functions
+// ****************************************************
+
+void UART_Init(){
+	/* Set baud rate */
+	UBRRH = (unsigned char)(UBRR>>8);
+	UBRRL = (unsigned char)UBRR;
+	/* Enable receiver and transmitter */
+	UCSRB = (1<<RXEN)|(1<<TXEN);
+	/* Set frame format: 8data, 2stop bit */
+	UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0);
+}
+
+void USART_Transmit( unsigned int data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSRA & (1<<UDRE)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR = data;
+}
+
+unsigned char USART_Receive()
+{
+	/* Wait for data to be received */
+	while ( !(UCSRA & (1<<RXC)) )
+	;
+	/* Get and return received data from buffer */
+	return UDR;
+}
+
+// ****************************************************
