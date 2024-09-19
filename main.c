a71078df4bcb5aeec887708ff1fd3004b53ea817
Fredrik Bratberg <fredrik.b.bratberg@gmail.com>
2024-09-19 13:05:21 +0200

    Made XMEM driver, SRAM test working

--- main.c
+++ main.c
@@ -10,9 +10,11 @@
 #define MYUBRR F_CPU/16/BAUD-1
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
+#include "xmem.h"
 
 // Test functions - prototypes
 // **************************************************
@@ -26,11 +28,19 @@ void UART_test();
 // Test the linking of printf (ATmega162)
 void PRINTF_test();
 
+// Testing the SRAM by writing and retrieving to the SRAM
+void SRAM_test();
+
 // **************************************************
 
 int main(void)
 {
-	 UART_test();
+	// Init drivers
+	UART_Init(MYUBRR);
+	XMEM_Init();
+	
+	// test shit
+	SRAM_test();
 }
 
  // Test functions - implementation
@@ -66,4 +76,38 @@ int main(void)
 	 printf("Testing linking of printf.");
  }
 
+void SRAM_test(void)
+{
+	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
+	uint16_t ext_ram_size = 0x800;
+	uint16_t write_errors = 0;
+	uint16_t retrieval_errors = 0;
+	printf("Starting SRAM test...\n");
+	// rand() stores some internal state, so calling this function in a loop will
+	// yield different seeds each time (unless srand() is called before this function)
+	uint16_t seed = rand();
+	// Write phase: Immediately check that the correct value was stored
+	srand(seed);
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		ext_ram[i] = some_value;
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			write_errors++;
+		}
+	}
+	// Retrieval phase: Check that no values were changed during or after the write phase
+	srand(seed);
+	// reset the PRNG to the state it had before the write phase
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			retrieval_errors++;
+		}
+	}
+	printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
+}
  // **************************************************
---
+++ xmem.h
@@ -0,0 +1,36 @@
+#ifndef xmem_H
+#define xmem_H
+
+// Includes
+// ****************************************************
+#include <stdint.h>
+// ****************************************************
+
+// Constants
+// ****************************************************
+#define BASE_ADDRESS 0x1800 // Base address of SRAM
+// ****************************************************
+
+// Types
+// ****************************************************
+// ****************************************************
+
+// Global variables
+// ****************************************************
+// ****************************************************
+
+// Function prototypes
+// ****************************************************
+
+// Enables external memory and masks the pins for JTAG
+void XMEM_Init();
+
+// Writes to BASE_ADDRESS + addr
+void XMEM_Write(uint8_t data, uint16_t addr);
+
+// Reads from BASE_ADDRESS + addr
+uint8_t XMEM_Read(uint16_t addr);
+
+// ****************************************************
+
+#endif
