cc4d1ea8024033fd3cbf5b9f9a9f4e3a48538a8c
Fredrik Bratberg <fredrik.b.bratberg@gmail.com>
2024-10-10 13:35:27 +0200

    mcp2515 driver complete, not tested

--- mcp2515.c
+++ mcp2515.c
@@ -3,6 +3,7 @@
 
 // Includes
 // ****************************************************
+#include "spi.h"
 // ****************************************************
 
 // Private macros and constants
@@ -23,4 +24,59 @@
 
 // Implementation of the public functions
 // ****************************************************
+
+void MCP2515_Init(){
+	MCP2515_Reset() ; // Send reset - command
+	
+	// More initialization?
+}
+
+void MCP2515_Reset(){
+	SPI_Ss(1);
+	SPI_Transmit(MCP_RESET);
+	SPI_Ss(0);
+}
+
+unsigned char MCP2515_Read(unsigned char address){
+	unsigned char result;
+	SPI_Ss(1);
+	SPI_Transmit(MCP_READ);
+	SPI_Transmit(address);
+	result = SPI_Receive();
+	SPI_Ss(0);
+	return result;
+}
+
+void MCP2515_Write(unsigned char address, unsigned char data){
+	SPI_Ss(1);
+	SPI_Transmit(MCP_WRITE);
+	SPI_Transmit(address);
+	SPI_Transmit(data);
+	SPI_Ss(0);
+}
+
+void MCP2515_RequestToSend(unsigned char RTS){
+	SPI_Ss(1);
+	SPI_Transmit(RTS);
+	SPI_Ss(0);
+}
+
+void MCP2515_BitModify(unsigned char address, unsigned char mask, unsigned char data){
+	SPI_Ss(1);
+	SPI_Transmit(MCP_BITMOD);
+	SPI_Transmit(address);
+	SPI_Transmit(mask);
+	SPI_Transmit(data);
+	SPI_Ss(0);
+}
+
+unsigned char MCP2515_ReadStatus(){
+	unsigned char status;
+	SPI_Ss(1);
+	SPI_Transmit(MCP_READ_STATUS);
+	status = SPI_Receive();
+	SPI_Ss(0);
+	return status;
+}
+
 // ****************************************************
--- mcp2515.h
+++ mcp2515.h
@@ -20,6 +20,35 @@
 // Function prototypes
 // ****************************************************
 
+// Initializes the mcp2515 driver
+// Call SPI_Init prior to this
+void MCP2515_Init();
+
+// Returns the value at a given address on the CAN controller
+// See mcp2515_regs.h for addresses
+unsigned char MCP2515_Read(unsigned char address);
+
+// Writes value at a given address on the CAN controller 
+// See mcp2515_regs.h for addresses 
+void MCP2515_Write(unsigned char address, unsigned char data);
+
+// Request instructs the controller to begin a message transmission
+// sequence for any of the transmit buffers. 
+// RTS: MCP_RTS_TX0-2 or  MCP_RTS_ALL found in mcp2515_regs.h
+void MCP2515_RequestToSend(unsigned char RTS);
+
+// Provides a means for setting or clearing individual bits in specific 
+// status and control registers. This command is not available for all
+// registers (see "register map" in datasheet)
+void MCP2515_BitModify(unsigned char address, unsigned char mask, unsigned char data);
+
+// Re-initializes the internal registers and set 
+// Configuration mode
+void MCP2515_Reset();
+
+// Returns the controller's status bits
+unsigned char MCP2515_ReadStatus();
+
 // ****************************************************
 
 #endif
