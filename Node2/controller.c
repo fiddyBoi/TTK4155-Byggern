c1eaeaa319e9d1c2c5f4820c699740660f2c9a56
Sondre Øgreid Vetti <sondrov@ntnu.no>
2024-11-14 14:36:04 +0100

    commited controller properly!!

--- Node2/controller.c
+++ Node2/controller.c
@@ -1,4 +1,4 @@
-#include "encoder.h"
+#include "controller.h"
 
 // Includes
 // ****************************************************
@@ -7,8 +7,6 @@
 
 // Private macros and constants
 // ****************************************************
-#define ENCODER_MAX (int32_t)5610
-#define ENCODER_HALF (int32_t)ENCODER_MAX/2
 // ****************************************************
 
 // Private types
@@ -17,6 +15,11 @@
 
 // Private global variables
 // ****************************************************
+int Kp;
+int Ki;
+int T = 20; // Sampling period ms
+int Integrator_value = 0;
+int reference = 0;
 // ****************************************************
 
 // Private functions
@@ -26,46 +29,47 @@
 // Implementation of the public functions
 // ****************************************************
 
-void ENCODER_Init(){
+void CONTROLLER_Init(int p, int i){
+	// Set regulator parameters
+	Kp = p; 
+	Ki = i; 
 	
-	// ENCODER
-	// encoder pins: PC25 D4, PC26 D5 (PA29)
-		
-	// Enable clock for PIOC
-	PMC->PMC_PCR |= PMC_PCR_EN | PMC_PCR_DIV_PERIPH_DIV_MCK | PMC_PCR_PID(ID_PIOC);
-	PMC->PMC_PCER0 |= (1 << ID_PIOC);
+	// Enable clock for RTT
+	PMC->PMC_PCR |= PMC_PCR_EN | PMC_PCR_DIV_PERIPH_DIV_MCK | PMC_PCR_PID(ID_RTT);
+	PMC->PMC_PCER0 |= (1 << ID_RTT);
 	
-	// Enable clock for TC2 
-	PMC->PMC_PCR |= PMC_PCR_EN | PMC_PCR_DIV_PERIPH_DIV_MCK | PMC_PCR_PID(ID_TC2);
-	// PMC->PMC_PCER0 |= (1 << ID_TC2);
-	PMC->PMC_PCER1 |= (1 << 1); // Mistake in datasheet??? TC2 is PID 29 and should use PCER0
+	// Configure RTT
+	RTT->RTT_MR = RTT_MR_RTPRES(32) | RTT_MR_ALMIEN; // 1khz, alarm mode
+	RTT->RTT_AR = RTT_AR_ALMV(T); // period
 	
-	// Select I/O to TC2 (peripheral B)
-	PIOC->PIO_ABSR |= PIO_ABSR_P25 | PIO_ABSR_P26;
-	PIOC->PIO_PDR |= PIO_PDR_P25 | PIO_PDR_P26; 
-	
-	// disable/enable clock for channel 0 during setup
-	TC2->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKDIS; 
-	
-	// Set block mode register for TC2
-	TC2->TC_BMR = TC_BMR_QDEN | TC_BMR_POSEN | TC_BMR_EDGPHA;
-	
-	// Set XC0 as the selected clock channel for channel 0
-	TC2->TC_CHANNEL[0].TC_CMR = TC_CMR_TCCLKS_XC0; 
-	
-	// disable/enable clock for channel 0 during setup
-	// SWTRG resets the counter and starts the clock
-	TC2->TC_CHANNEL[0].TC_CCR |=  TC_CCR_CLKEN | TC_CCR_SWTRG; 
+	// Enable interrupt
+	NVIC_EnableIRQ(ID_RTT);
 }
 
-uint32_t ENCODER_Read(){
-	return TC2->TC_CHANNEL[0].TC_CV;
+int PI_Controller(int reference, int measurement){
+	int error = reference - measurement;
+	Integrator_value += error; 
+	int output = Kp*error + T/1000*Ki*Integrator_value;
+	return output;
 }
 
-int ENCODER_Scale(uint32_t encoder_value){
-	// -ENCODER_MAX/2 -> -100,  ENCODER_MAX/2 -> 100
-	int32_t data = (int32_t) encoder_value;
-	return 100*data/(ENCODER_HALF);
+void CONTROLLER_SetReference(int r){
+	reference = r; 
 }
+
+void RTT_Handler(void){
+	// Read status register 
+	uint32_t rtt_sr = RTT->RTT_SR;
+	// Measure motor position 
+	int measurement = ENCODER_Scale(ENCODER_Read());
+	// Calculate output
+	int output = PI_Controller(reference, measurement);
+	// Control the motor
+	MOTOR_Output(output);
+	// Reset clock
+	RTT->RTT_MR |= RTT_MR_RTTRST;
+	// Clear
+	NVIC_ClearPendingIRQ(ID_RTT);
+};
 
 // ****************************************************
--- Node2/controller.h
+++ Node2/controller.h
@@ -1,8 +1,7 @@
-#ifndef encoder_H
-#define encoder_H
+#ifndef controller_H
+#define controller_H
 // Includes
 // ****************************************************
-#include <stdio.h>
 // ****************************************************
 
 // Constants
@@ -22,13 +21,16 @@
 // ****************************************************
 
 // We assume the motor starts in the middle position!
-void ENCODER_Init();
+// Initializes the motor PI controller, p and i are 
+// regulator parameters. 
+// When started in the middle position the encoder
+// measurement will yield positions in the range 
+// -100 to 100 where 0 is the middle position.
+void CONTROLLER_Init(int p, int i);
 
-uint32_t ENCODER_Read();
-
-// Converts the encoder measurement to an -100 to 100 
-// Interval
-int ENCODER_Scale(uint32_t encoder_value);
+// The joysticks -100 to 100 signal is used as a reference for
+// the motor position. Call this function to update the reference
+void CONTROLLER_SetReference(int r);
 
 // ****************************************************
 #endif
